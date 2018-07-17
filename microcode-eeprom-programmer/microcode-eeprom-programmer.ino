e32a8bbcfe00c65eb0ad6631425dc81601cb661d
Matt Cosand <matt@cosand.net>
2018-07-16 18:48:24 -0700

    Microcode

--- microcode-eeprom-programmer/microcode-eeprom-programmer.ino
+++ microcode-eeprom-programmer/microcode-eeprom-programmer.ino
@@ -8,40 +8,63 @@
 #define EEPROM_D0 5
 #define EEPROM_D7 12
 #define WRITE_EN 13
+#define LAST_ADDRESS_BIT 12
+#define CHIP_OFFSET (1 << LAST_ADDRESS_BIT)
 
-#define HLT 0b1000000000000000  // Halt clock
-#define MI  0b0100000000000000  // Memory address register in
-#define RI  0b0010000000000000  // RAM data in
-#define RO  0b0001000000000000  // RAM data out
-#define IO  0b0000100000000000  // Instruction register out
-#define II  0b0000010000000000  // Instruction register in
-#define AI  0b0000001000000000  // A register in
-#define AO  0b0000000100000000  // A register out
-#define EO  0b0000000010000000  // ALU out
-#define SU  0b0000000001000000  // ALU subtract
-#define BI  0b0000000000100000  // B register in
-#define OI  0b0000000000010000  // Output register in
-#define CE  0b0000000000001000  // Program counter enable
-#define CO  0b0000000000000100  // Program counter out
-#define J   0b0000000000000010  // Jump (program counter in)
+/* Data sheet is wrong!! Fix it in software!!
+#define CE  0b1000000000000000  // Program counter enable
+#define CO  0b0100000000000000  // Program counter out
+#define IO  0b0010000000000000  // Instruction register out
+#define II  0b0001000000000000  // Instruction register in
+#define RO  0b0000100000000000  // RAM data out
+*/
+#define CE  0b0000100000000000  // Program counter enable
+#define CO  0b0001000000000000  // Program counter out
+#define IO  0b0010000000000000  // Instruction register out
+#define II  0b0100000000000000  // Instruction register in
+#define RO  0b1000000000000000  // RAM data out
+#define RI  0b0000010000000000  // RAM data in
+#define MI  0b0000001000000000  // Memory address register in
+#define HLT 0b0000000100000000  // Halt clock
+
+/* Data sheet is wrong!! Fix it in software!!
+#define OI  0b0000000010000000  // Output register in
+#define OP  0b0000000001000000  // ALU subtract
+#define EO  0b0000000000100000  // ALU out
+#define BO  0b0000000000010000  // B register out
+#define BI  0b0000000000001000  // B register in
+*/
+#define OI  0b0000000000001000  // Output register in
+#define OP  0b0000000000010000  // ALU subtract
+#define EO  0b0000000000100000  // ALU out
+#define BO  0b0000000001000000  // B register out
+#define BI  0b0000000010000000  // B register in
+#define AO  0b0000000000000100  // A register out
+#define AI  0b0000000000000010  // A register in
+#define J   0b0000000000000001  // Jump (program counter in)
+#define CI J
+
+#define ADDR_HACK(a) (((a) & 0b111) | (((a) & 0b1111000) << 1))
+
+uint16_t ACTIVE_LOWS = (CO|IO|II|RO|MI|EO|BO|BI|AO|AI|CI);
 
 uint16_t data[] = {
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 0000 - NOP
-  MI|CO,  RO|II|CE,  IO|MI,  RO|AI,  0,         0, 0, 0,   // 0001 - LDA
-  MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI,     0, 0, 0,   // 0010 - ADD
-  MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI|SU,  0, 0, 0,   // 0011 - SUB
-  MI|CO,  RO|II|CE,  IO|MI,  AO|RI,  0,         0, 0, 0,   // 0100 - STA
-  MI|CO,  RO|II|CE,  IO|AI,  0,      0,         0, 0, 0,   // 0101 - LDI
-  MI|CO,  RO|II|CE,  IO|J,   0,      0,         0, 0, 0,   // 0110 - JMP
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 0111
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1000
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1001
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1010
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1011
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1100
-  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1101
-  MI|CO,  RO|II|CE,  AO|OI,  0,      0,         0, 0, 0,   // 1110 - OUT
-  MI|CO,  RO|II|CE,  HLT,    0,      0,         0, 0, 0,   // 1111 - HLT
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 0000 - NOP
+  MI|CO,  RO|II|CE,  IO|MI,  RO|AI,  0,         0, 0,0,   // 0001 - LDA
+  MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI,     0, 0,0,   // 0010 - ADD
+  MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI|OP,  0, 0,0,   // 0011 - SUB
+  MI|CO,  RO|II|CE,  IO|MI,  AO|RI,  0,         0, 0,0,   // 0100 - STA
+  MI|CO,  RO|II|CE,  IO|AI,  0,      0,         0, 0,0,   // 0101 - LDI
+  MI|CO,  RO|II|CE,  IO|J,   0,      0,         0, 0,0,   // 0110 - JMP
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 0111
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 1000
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 1001
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 1010
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 1011
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 1100
+  MI|CO,  RO|II|CE,  0,      0,      0,         0, 0,0,   // 1101
+  MI|CO,  RO|II|CE,  AO|OI,  0,      0,         0, 0,0,   // 1110 - OUT
+  MI|CO,  RO|II|CE,  HLT,    0,      0,         0, 0,0,   // 1111 - HLT
 };
 
 
@@ -125,26 +148,36 @@ void setup() {
   Serial.begin(57600);
 
   // Program data bytes
-  Serial.print("Programming EEPROM");
+  Serial.println("Programming EEPROM");
 
-  // Program the 8 high-order bits of microcode into the first 128 bytes of EEPROM
   for (int address = 0; address < sizeof(data)/sizeof(data[0]); address += 1) {
-    writeEEPROM(address, data[address] >> 8);
+    // High order bits
+    writeEEPROM(ADDR_HACK(address) + CHIP_OFFSET, (ACTIVE_LOWS ^ data[address]) >> 8);
+    Serial.print(ADDR_HACK(address) + CHIP_OFFSET);
+    Serial.print(":");
+    Serial.print((ACTIVE_LOWS ^ data[address]) >> 8);
+    Serial.print(" ");
 
-    if (address % 64 == 0) {
+    writeEEPROM(ADDR_HACK(address), (ACTIVE_LOWS ^ data[address]) & 0xFF);
+    Serial.print(ADDR_HACK(address));
+    Serial.print(":");
+    Serial.println((ACTIVE_LOWS ^ data[address]) & 0xFF);
+
+    if (address % 32 == 0) {
       Serial.print(".");
     }
   }
 
-  // Program the 8 low-order bits of microcode into the second 128 bytes of EEPROM
-  for (int address = 0; address < sizeof(data)/sizeof(data[0]); address += 1) {
-    writeEEPROM(address + 128, data[address]);
-
+  // Program the 8 low-order bits of microcode into the second half of EEPROM
+  /*
+  for (uint16_t address = 0; address < sizeof(data)/sizeof(data[0]); address += 1) {
+    //writeEEPROM(ADDR_HACK(address + (1 << LAST_ADDRESS_BIT)), (ACTIVE_LOWS & 0xFF) ^ (data[address] & 0xFF));
     if (address % 64 == 0) {
       Serial.print(".");
     }
+    
   }
-
+  */
   Serial.println(" done");
 
 
